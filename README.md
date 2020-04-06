68aa271946fe0008a2f38ba80a49de8da1a74d34
Ben Reynolds <breynolds.ctr@kr.af.mil>
2020-04-06 17:55:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# INFO 6205 Final Project
+
+This application is a ranking system capable of predicting league standings for the English Premier League based on past match outcomes.
---
+++ src/main/java/com/renbeynolds/eplranking/EPLRanking.java
@@ -0,0 +1,7 @@
+package com.renbeynolds.eplranking;
+
+public class EPLRanking {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
